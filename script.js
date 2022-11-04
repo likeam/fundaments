ee2bc13644432348f4db2dade187969cd98a0f0d
AbdulRehman <likeam99@gmail.com>
2022-11-04 22:42:53 +0500

    Arrow function

--- script.js
+++ script.js
@@ -6,16 +6,23 @@
 // if(passTest) hasDriversLicence = true;
 // if(hasDriversLicence) console.log('I can drive :D');
 
-function logger() {
-    console.log('My name is A Rehman');
-}
-logger();
-logger();
+// function logger() {
+//     console.log('My name is A Rehman');
+// }
+// logger();
+// logger();
 
-function fruitProcessor(apples, oranges){
-    console.log(apples, oranges);
-    const juice = `Juices with ${apples} apples and ${oranges} oranges`;
-    return juice;
+// function fruitProcessor(apples, oranges){
+//     console.log(apples, oranges);
+//     const juice = `Juices with ${apples} apples and ${oranges} oranges`;
+//     return juice;
+// }
+// const appleJuices = fruitProcessor(5, 0);
+// console.log(appleJuices);
+
+
+const calcAge = function(birthYear){
+    return 2037 - birthYear;
 }
-const appleJuices = fruitProcessor(5, 0);
-console.log(appleJuices);
+
+birthYear => 2037 - birthYear;
